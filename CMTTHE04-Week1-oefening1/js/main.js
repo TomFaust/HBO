b5e80f0c249b61158eac3c80b87bd6e57553b468
Tom Faust <tom.faust@hotmail.com>
2020-05-12 14:28:59 +0200

    hi

--- CMTTHE04-Week1-oefening1/js/main.js
+++ CMTTHE04-Week1-oefening1/js/main.js
@@ -2,13 +2,8 @@
 // game element
 let game = document.getElementsByTagName("game")[0]
 
-function placeElement(){
-
-    let i;
-    for (i = 0; i < 100; i++) {
-
-        // vis element
-        let xFish = Math.floor(Math.random() * 100);
+function createFish(){
+    let xFish = Math.floor(Math.random() * 100);
         let yFish = Math.floor(Math.random() * 100);
         let colorFish = Math.floor(Math.random() * 360);
 
@@ -21,17 +16,26 @@ function placeElement(){
             fish.classList.add("dead")
         }
         )
+}
 
-        // bubble element
-        let xBubble = Math.floor(Math.random() * 100);
-        let yBubble = Math.floor(Math.random() * 100);
+function createBubble(){
+    // bubble element
+    let xBubble = Math.floor(Math.random() * 100);
+    let yBubble = Math.floor(Math.random() * 100);
 
-        let bubble = document.createElement("bubble")
-        bubble.style.transform = `translate(${xBubble}vw, ${yBubble}vh )`
-        game.appendChild(bubble)
-        bubble.addEventListener('click',function(){
-            bubble.remove()
-        })
+    let bubble = document.createElement("bubble")
+    bubble.style.transform = `translate(${xBubble}vw, ${yBubble}vh )`
+    game.appendChild(bubble)
+    bubble.addEventListener('click',function(){
+        bubble.remove()
+    })
+}
+
+function placeElement(){
+    let i;
+    for (i = 0; i < 100; i++) {
+        createBubble()
+        createFish()
     }
 
 }
